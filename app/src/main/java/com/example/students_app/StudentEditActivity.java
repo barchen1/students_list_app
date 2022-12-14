7975a87759f4644a33b2662b0ed8e2af3adaee4c
barchen <barchen7200@gmail.com>
2022-12-14 20:05:15 +0200

    clean code

--- app/src/main/java/com/example/students_app/StudentEditActivity.java
+++ app/src/main/java/com/example/students_app/StudentEditActivity.java
@@ -2,7 +2,6 @@ package com.example.students_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
